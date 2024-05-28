853feb5c13c0c624f28751a5ac826560cd10eea6
cookingnoob <costealan@gmail.com>
2024-05-27 19:23:26 -0600

    add copy titles and some styles

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import NameTitle from '../components/NameTitle'
+import { Box, Typography } from '@mui/material'
 
 const Welcome = () => {
   return (
-    <NameTitle />
+    <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+      <NameTitle />
+      <Typography variant='h1'>Lorem</Typography>
+      <Typography variant='h1' color={'rgba(221, 64, 64, 1)'}>Lorem</Typography>
+      <Typography variant='h1' color={'rgba(221, 64, 64, 1)'}>Lorem</Typography>
+      <Typography variant='h1'>Lorem</Typography>
+      <Typography variant='h1'>Lorem</Typography>
+    </Box>
   )
 }
 
--- src/pages/Welcome.test.jsx
+++ src/pages/Welcome.test.jsx
@@ -1,12 +1,19 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { describe, expect, it, test } from "vitest";
+import { afterEach, describe, expect, it, test } from "vitest";
 import Welcome from "./Welcome";
+afterEach(cleanup);
 
 describe('render elements from Welcome Page', () => {
+
   test('renders name', () => {
     render(<Welcome />)
     const name = screen.getByRole('heading', { name: /Alan Coste/i })
     expect(name).toBeInTheDocument()
+  });
+  test('renders copy titles', () => {
+    render(<Welcome />)
+    const copy = screen.getAllByRole('heading', { name: /Lorem/i })
+    expect(copy.length).toBeGreaterThan(4)
   })
 })
