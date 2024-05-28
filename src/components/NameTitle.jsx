18a80b504455f46d699da7c65c520a2c6437bd05
cookingnoob <costealan@gmail.com>
2024-05-27 22:59:23 -0600

    add theme and styles for welcome page

--- src/components/NameTitle.jsx
+++ src/components/NameTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const NameTitle = () => {
   return (
-    <Typography variant='h3'>Alan Coste</Typography>
+    <Typography variant='h5' color={'primary'}>Alan Coste</Typography>
   )
 }
 
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import NameTitle from '../components/NameTitle'
 import { Box, Typography } from '@mui/material'
+import { useTheme } from '@emotion/react'
 
 const Welcome = () => {
+  const theme = useTheme()
   return (
-    <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+    <Box display={'flex'}
+      flexDirection={'column'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      height={'100vh'}
+      width={'100vw'}
+      bgcolor={'primary.dark'}>
       <NameTitle />
-      <Typography variant='h1'>Lorem</Typography>
-      <Typography variant='h1' color={'rgba(221, 64, 64, 1)'}>Lorem</Typography>
-      <Typography variant='h1' color={'rgba(221, 64, 64, 1)'}>Lorem</Typography>
-      <Typography variant='h1'>Lorem</Typography>
-      <Typography variant='h1'>Lorem</Typography>
+      <Typography variant='h1' color={'primary'}>Lorem</Typography>
+      <Typography variant='h1' color={'secondary'}>Lorem</Typography>
+      <Typography variant='h1' color={'secondary'}>Lorem</Typography>
+      <Typography variant='h1' color={'primary'}>Lorem</Typography>
+      <Typography variant='h1' color={'primary'}>Lorem</Typography>
     </Box>
   )
 }
