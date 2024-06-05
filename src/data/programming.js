a3e1a4fedccaef22cebf417883d305f0415d7aac
cookingnoob <costealan@gmail.com>
2024-06-04 19:08:47 -0600

    add a programming file that handles an array of my programming projects.
    deleted the programming projects from the Programming page

---
+++ src/data/programming.js
@@ -0,0 +1,128 @@
+export const programmingExp = [
+  {
+    tech: ['React'],
+    title: 'Weather App',
+    description: 'A React SPA that fetches information from Open Weather’s API. It displays the current weather and a five-day forecast for the user’s location and five different cities. The background image changes dynamically based on weather data, with images created using Midjourney.',
+    projectUrl: 'https://rtc-weatherapp.netlify.app/',
+    repository: 'https://github.com/cookingnoob/rtc-react-weatherApp'
+  },
+  {
+    tech: ['React', 'Typescript', 'MUI', 'Vitest', 'React-Testing-Library'],
+    title: 'IberiViaje',
+    description: 'A React SPA made with TypeScript and Material UI, tested using Vitest and React Testing Library. It is a mock site for a travel agency located in Spain, with images taken by me.',
+    projectUrl: 'https://marvelous-gelato-277f04.netlify.app',
+    repository: 'https://github.com/cookingnoob/rtc-react-styles-tests-typescript'
+  },
+  {
+    tech: ['React'],
+    title: 'Board Games: tic-tac-toe, sudoku, hangman',
+    description: 'A React SPA where users can play board games like Hangman, Tic Tac Toe, and Sudoku. This project focuses on utilizing custom hooks and clean code practices. It includes a basic user log feature where players can see their chosen names rendered.',
+    projectUrl: 'https://rtc-games.netlify.app',
+    repository: 'https://github.com/cookingnoob/rtc-games'
+  },
+  {
+    tech: ['React'],
+    title: 'Nasa daily images',
+    description: 'My first React App. It fetches images from the NASA picture of the day API and the Rover API. ',
+    projectUrl: 'https://rtc-nasa-alan-coste.netlify.app',
+    repository: 'https://github.com/cookingnoob/rtc-nasa'
+  },
+  {
+    tech: ['Nodejs', 'Express', 'JWT', 'MongoDB', 'Cloudinary'],
+    title: 'To-do list API',
+    description: 'API for my first fullstack project. Users can add avatars, create lists, and manage to-dos within those lists (default lists include General and Priority). The frontend development for this project is currently a work in progress.',
+    projectUrl: 'https://to-do-api-e4u1.onrender.com',
+    repository: 'https://github.com/cookingnoob/rtc-final-server'
+  },
+  {
+    tech: ['Nodejs', 'Express', 'JWT', 'MongoDB', 'Cloudinary'],
+    title: 'Cookbooks API',
+    description: 'Cookbooks API where user can create an account to handle CRUD operations for the cookbooks, their recipes or even upload an avatar.',
+    projectUrl: 'https://rtc-backend3.onrender.com',
+    repository: 'https://github.com/cookingnoob/rtc-backend3'
+  },
+  {
+    tech: ['Nodejs', 'Express', 'MongoDB'],
+    title: 'Videogames API',
+    description: 'Videogame API that handles a 1:n relationship. CRUD operations for new consoles and videogames',
+    projectUrl: 'https://github.com/cookingnoob/rtc-backend2',
+    repository: 'https://github.com/cookingnoob/rtc-backend2'
+  },
+  {
+    tech: ['Nodejs', 'Express', 'MongoDB'],
+    title: 'Albums API',
+    description: 'First backend project. It is a MongoDB that has a 1:n relationship. It handles the songs in an album',
+    projectUrl: 'https://github.com/cookingnoob/rtc-backend1',
+    repository: 'https://github.com/cookingnoob/rtc-backend1'
+  },
+  {
+    tech: 'HTML',
+    title: 'Recipe blog',
+    description: 'My first page ever! A simple food blog that renders my three favorite recipes. Images and recipes are mine!',
+    projectUrl: 'https://cookingnoob.github.io/odin-recipes/recipes/ribeye.html',
+    repository: 'https://github.com/cookingnoob/odin-recipes'
+  },
+  {
+    tech: ['HTML', 'CSS'],
+    title: 'Landing page',
+    description: 'Basic website using flexbox to layout the elements',
+    projectUrl: 'https://cookingnoob.github.io/landing-page-top-flexbox/',
+    repository: 'https://github.com/cookingnoob/landing-page-top-flexbox'
+  },
+  {
+    tech: ['HTML', 'CSS'],
+    title: 'Copycat of Tech store',
+    description: 'Web responsive tech store. It is as close as possible to the original tech store',
+    projectUrl: 'https://rtc-webresponsive-alancoste.netlify.app/',
+    repository: 'https://github.com/cookingnoob/web-responsive-rtc'
+  },
+  {
+    tech: ['HTML', 'CSS'],
+    title: 'Sign Up Form',
+    description: 'Simple sign up page made with CSS and HTML',
+    projectUrl: 'https://cookingnoob.github.io/sign-up-form-odin/',
+    repository: 'https://github.com/cookingnoob/sign-up-form-odin'
+  },
+  {
+    tech: ['Javascript', 'HTML', 'CSS'],
+    title: 'Tech store with filter by price and brand option',
+    description: 'Javascript project where a list of computers have to be filtered by price or brand name',
+    projectUrl: 'https://bejewelled-dodol-5a9c07.netlify.app',
+    repository: 'https://github.com/cookingnoob/rtc-tienda'
+  },
+  {
+    tech: ['Javascript', 'HTML', 'CSS'],
+    title: 'Dummy portfolio',
+    description: 'SPA Portfolio made only with Javascript, there are factories to create different DOM elements. Text content was created with ChatGPT, images with Midjourney',
+    projectUrl: 'https://rtc-portafoliovite.netlify.app',
+    repository: 'https://github.com/cookingnoob/rtc-vite-portafolio'
+  },
+  {
+    tech: ['Javascript', 'CSS', 'HTML'],
+    title: 'Calculator',
+    description: 'First Javascript with no guidance. I made a refactor to delete unnecessary functions and clean the code',
+    projectUrl: 'https://cookingnoob.github.io/calculator/',
+    repository: 'https://github.com/cookingnoob/calculator'
+  },
+  {
+    tech: ['Javascript', 'CSS', 'HTML'],
+    title: 'Etch a Sketch',
+    description: 'Etch a sketch game, it changes the color of the cell when the cursor hovers over it',
+    projectUrl: 'https://cookingnoob.github.io/etch-a-sketch',
+    repository: 'https://github.com/cookingnoob/etch-a-sketch'
+  },
+  {
+    tech: ['Javascript', 'HTML'],
+    title: 'Rock Paper Scissors',
+    description: 'Rock paper scissors. The first time my Javascript code interacted with DOM elements',
+    projectUrl: 'https://cookingnoob.github.io/rock-paper-scissors2/',
+    repository: 'https://github.com/cookingnoob/rock-paper-scissors2'
+  },
+  {
+    tech: ['Typescript'],
+    title: 'Library',
+    description: 'Javascript page for adding books to a list. There is a factory of books asking for author, title, number of pages and if user read the book. I made a refactor of the code adding Typescript and simplified the code making it cleaner.',
+    projectUrl: 'https://github.com/cookingnoob/libraryOdin',
+    repository: 'https://github.com/cookingnoob/libraryOdin'
+  },
+]
--- src/pages/Programming.jsx
+++ src/pages/Programming.jsx
@@ -5,12 +5,7 @@ import { Box, Typography } from '@mui/material'
 const Programming = () => {
   return (
     <>
-      {/* tech template
-      HTML
-      CSS
-      JS
-      React
-      Node y express */}
+
       <Typography variant='h1' color={'primary'}>React:</Typography>
 
       <Box display={'flex'} flexDirection={'column'} alignItems={'center'} gap={'5px'} justifyContent={'center'}>
@@ -19,45 +14,11 @@ const Programming = () => {
           <Typography variant='h3' color={'primary'}>Weather app</Typography>
           <Typography color={'primary'}>React app that fetches information from weatherAPI. It gets local weather with users permission. There is a dynamic image for the background to ilustrate hows the current climate. User can check 5 different cities for their current weather or within 5 days. It is deployed on Netlify</Typography>
           <Microlink url='https://rtc-weatherapp.netlify.app/' media='screenshot' />
+          <Microlink url='https://github.com/cookingnoob/rtc-react-weatherApp' />
         </Box>
 
-        <Microlink url=' https://marvelous-gelato-277f04.netlify.app/' media='screenshot' />
-        <Microlink url='https://rtc-games.netlify.app' media='screenshot' />
-        <Microlink url='https://rtc-nasa-alan-coste.netlify.app' media='screenshot' />
       </Box>
-      <Typography variant='h1' color={'primary'}>Nodejs | Express:</Typography>
-      <Box display={'flex'} flexDirection={'column'} alignItems={'center'} gap={'5px'} justifyContent={'center'}>
-        <Microlink url='https://rtc-backend3.onrender.com' />
-        <Microlink url=' https://github.com/cookingnoob/rtc-backend2' media='logo' />
-        <Microlink url='https://github.com/cookingnoob/rtc-final-server' media='logo' />
-        <Microlink url='https://github.com/cookingnoob/rtc-backend1' media='logo' />
-      </Box>
-      <Typography variant='h1' color={'primary'}>HTML5 || CSS</Typography>
-      <Box>
-        <Microlink url='https://github.com/cookingnoob/odin-recipes' />
-        <Microlink url='https://github.com/cookingnoob/admin-dashboard-odin' />
-        <Microlink url='https://github.com/cookingnoob/landing-page-top-flexbox' />
-        <Microlink url=' https://github.com/cookingnoob/web-responsive-rtc' />
-        <Microlink url='  https://github.com/cookingnoob/sign-up-form-odin' />
 
-
-      </Box>
-      <Typography variant='h1' color={'primary'}>Javascript</Typography>
-      <Box>
-        <Microlink url='https://github.com/cookingnoob/rtc-tienda' />
-        <Microlink url='https://github.com/cookingnoob/rtc-vite-portafolio' />
-        <Microlink url='https://github.com/cookingnoob/calculator' />
-        <Microlink url='https://github.com/cookingnoob/etch-a-sketch' />
-        <Microlink url=' https://github.com/cookingnoob/rockpaperscissors' />
-
-
-      </Box>
-      <Typography variant='h1' color={'primary'}>Typescript</Typography>
-      <Box>
-        <Microlink url='https://github.com/cookingnoob/rtc-react-styles-tests-typescript' />
-        <Microlink url='https://github.com/cookingnoob/libraryOdin' />
-        <Microlink url='https://github.com/cookingnoob/calculator' />
-      </Box>
 
     </>
 
