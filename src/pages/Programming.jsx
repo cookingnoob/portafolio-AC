a3d575d18983ed5daba5492f9fe7b7f77f81356a
cookingnoob <costealan@gmail.com>
2024-06-06 10:42:14 -0600

    add creative projects array, it is just a template, no info at the moment

--- src/pages/Programming.jsx
+++ src/pages/Programming.jsx
@@ -5,21 +5,6 @@ import { Box, Typography } from '@mui/material'
 const Programming = () => {
   return (
     <>
-
-      <Typography variant='h1' color={'primary'}>React:</Typography>
-
-      <Box display={'flex'} flexDirection={'column'} alignItems={'center'} gap={'5px'} justifyContent={'center'}>
-        {/* project card */}
-        <Box>
-          <Typography variant='h3' color={'primary'}>Weather app</Typography>
-          <Typography color={'primary'}>React app that fetches information from weatherAPI. It gets local weather with users permission. There is a dynamic image for the background to ilustrate hows the current climate. User can check 5 different cities for their current weather or within 5 days. It is deployed on Netlify</Typography>
-          <Microlink url='https://rtc-weatherapp.netlify.app/' media='screenshot' />
-          <Microlink url='https://github.com/cookingnoob/rtc-react-weatherApp' />
-        </Box>
-
-      </Box>
-
-
     </>
 
   )
