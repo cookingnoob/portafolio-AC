a93beefe93b101d4b98dfeb99735e45c1a81d573
cookingnoob <costealan@gmail.com>
2024-06-06 17:21:18 -0600

    add filter to search projects by technology

--- src/components/CodingProjectsCard.jsx
+++ src/components/CodingProjectsCard.jsx
@@ -1,27 +1,43 @@
 import { Box, Button, Link, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { programmingExp } from '../data/programming'
 import Microlink from '@microlink/react'
 
 const CodingProjectsCard = () => {
+  const [projects, setProjects] = useState(programmingExp)
 
+
+  const handleFilter = (tech) => {
+    setProjects(programmingExp.filter(p => p.tech.includes(tech)))
+  }
   return (
+    //all cards container
     <Box display={'grid'} gridTemplateColumns={'repeat(3, 1fr)'} height={'80vh'} width={'90vw'} overflow={'scroll'} gap={'10px'} padding={'5px'}>
-      {programmingExp.map((p, index) => (
-        <Box key={`${p}-${index}`} bgcolor={'primary.main'} height={'300px'}>
+      {projects.map((p, index) => (
+        //individual card
+        <Box key={`${p}-${index}`} display={'flex'} flexDirection={'column'} bgcolor={'primary.main'} height={'300px'} padding={'5px'}>
           <Typography color={'primary.dark'}>{p.title}</Typography>
-          {p.tech.map((t, index) => (
-            <Button key={`${t}-${index}`} size='20px' variant='outlined'>
-              <br />
-              <Typography color={'primary.dark'} fontSize={'10px'}>{t}</Typography>
-            </Button>
-          ))}
+          <Box display={'flex'} alignItems={'center'} justifyContent={'center'} >
+            {p.tech.map((t, index) => (
+              <Button size='small' key={`${t}-${index}`} variant='outlined' onClick={() => handleFilter(t)}>
+                <br />
+                <Typography color={'primary.dark'} fontSize={'10px'}>{t}</Typography>
+              </Button>
+            ))}
+
+          </Box>
           <br />
-          <Typography variant='parragraph' color={'primary.dark'} fontSize={'12px'}>{p.description}</Typography>
+          <Box height={'100px'}>
+            <Typography variant='parragraph' color={'primary.dark'} fontSize={'12px'}>{p.description}</Typography>
+
+          </Box>
           <Microlink url={p.projectUrl} lazy={{ threshold: 0.5 }} fetchData />
-          <Link href={`${p.repository}`}>
-            <img width={'20px'} height={'20px'} src='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png' />
-          </Link>
+          <Box display={'flex'} >
+            <Typography color={'primary.dark'}>Repo:</Typography>
+            <Link href={`${p.repository}`}>
+              <img width={'40px'} height={'40px'} src='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png' />
+            </Link>
+          </Box>
         </Box>
       ))}
     </Box>
