a0ae21ea339b19169a8048bfb88039e43acbc24d
cookingnoob <costealan@gmail.com>
2024-06-07 18:38:25 -0600

    add buttons to render either tiktok, cooking or photography content

--- src/pages/Creative.jsx
+++ src/pages/Creative.jsx
@@ -1,5 +1,5 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import Microlink from '@microlink/react'
 import { TikTokEmbed } from 'react-social-media-embed';
 import ReelModal from '../components/ReelModal';
@@ -7,19 +7,31 @@ import { reels } from '../data/creative';
 
 const Creative = () => {
 
-  reels.map(r => console.log(r))
+  const [displayContent, setDisplayContent] = useState(null)
+
   return (
     <Box
       bgcolor={'primary.dark'}
       display={'flex'}
+      flexDirection={'column'}
       justifyContent={'center'}
+      alignItems={'center'}
     >
-      <Box display={'grid'} gridTemplateColumns={'repeat(3, 1fr)'} gap={'30px'}>
-        {reels.map((r, index) => (
-          <ReelModal key={`${r}-${index}`} reel={r} />
-        ))}
-      </Box>
+      <Box >
 
+        <Button onClick={() => setDisplayContent('tiktok')}>tiktok</Button>
+        <Button onClick={() => setDisplayContent('cooking')}>Cooking</Button>
+        <Button onClick={() => setDisplayContent('photography')}>Photography</Button>
+      </Box>
+      {displayContent !== 'tiktok' ? <></> :
+        <Box display={'grid'} gridTemplateColumns={'repeat(3, 1fr)'} gap={'30px'}>
+          {reels.map((r, index) => (
+            <ReelModal key={`${r}-${index}`} reel={r} />
+          ))}
+        </Box>
+      }
+      {displayContent !== 'cooking' ? <></> : <p>cooking</p>}
+      {displayContent !== 'photography' ? <></> : <p>photography</p>}
     </Box>
   )
 }
