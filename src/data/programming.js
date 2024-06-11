c39b65de48af1a086e40b3556db2915f73e19e01
cookingnoob <costealan@gmail.com>
2024-06-11 13:05:23 -0600

    add more info and less info to the about page

--- src/data/programming.js
+++ src/data/programming.js
@@ -1,12 +1,5 @@
 export const programmingExp = [
   {
-    tech: ['React'],
-    title: 'Weather App',
-    description: 'A React SPA that fetches information from Open Weather’s API. It displays the current weather and a five-day forecast for the user’s location and five different cities. The background image changes dynamically based on weather data, with images created using Midjourney.',
-    projectUrl: 'https://rtc-weatherapp.netlify.app/',
-    repository: 'https://github.com/cookingnoob/rtc-react-weatherApp'
-  },
-  {
     tech: ['React', 'Typescript', 'MUI', 'Vitest', 'React-Testing-Library'],
     title: 'IberiViaje',
     description: 'A React SPA made with TypeScript and Material UI, tested using Vitest and React Testing Library. It is a mock site for a travel agency located in Spain, with images taken by me.',
@@ -19,6 +12,20 @@ export const programmingExp = [
     description: 'A React SPA where users can play board games like Hangman, Tic Tac Toe, and Sudoku. This project focuses on utilizing custom hooks and clean code practices. It includes a basic user log feature where players can see their chosen names rendered.',
     projectUrl: 'https://rtc-games.netlify.app',
     repository: 'https://github.com/cookingnoob/rtc-games'
+  },
+  {
+    tech: ['Nodejs', 'Express', 'JWT', 'MongoDB', 'Cloudinary'],
+    title: 'Cookbooks API',
+    description: 'Cookbooks API where user can create an account to handle CRUD operations for the cookbooks, their recipes or even upload an avatar.',
+    projectUrl: 'https://rtc-backend3.onrender.com',
+    repository: 'https://github.com/cookingnoob/rtc-backend3'
+  },
+  {
+    tech: ['React'],
+    title: 'Weather App',
+    description: 'A React SPA that fetches information from Open Weather’s API. It displays the current weather and a five-day forecast for the user’s location and five different cities. The background image changes dynamically based on weather data, with images created using Midjourney.',
+    projectUrl: 'https://rtc-weatherapp.netlify.app/',
+    repository: 'https://github.com/cookingnoob/rtc-react-weatherApp'
   },
   {
     tech: ['React'],
@@ -34,13 +41,7 @@ export const programmingExp = [
     projectUrl: 'https://to-do-api-e4u1.onrender.com',
     repository: 'https://github.com/cookingnoob/rtc-final-server'
   },
-  {
-    tech: ['Nodejs', 'Express', 'JWT', 'MongoDB', 'Cloudinary'],
-    title: 'Cookbooks API',
-    description: 'Cookbooks API where user can create an account to handle CRUD operations for the cookbooks, their recipes or even upload an avatar.',
-    projectUrl: 'https://rtc-backend3.onrender.com',
-    repository: 'https://github.com/cookingnoob/rtc-backend3'
-  },
+
   {
     tech: ['Nodejs', 'Express', 'MongoDB'],
     title: 'Videogames API',
@@ -55,33 +56,13 @@ export const programmingExp = [
     projectUrl: 'https://github.com/cookingnoob/rtc-backend1',
     repository: 'https://github.com/cookingnoob/rtc-backend1'
   },
+
   {
-    tech: ['HTML'],
-    title: 'Recipe blog',
-    description: 'My first page ever! A simple food blog that renders my three favorite recipes. Images and recipes are mine!',
-    projectUrl: 'https://cookingnoob.github.io/odin-recipes/recipes/ribeye.html',
-    repository: 'https://github.com/cookingnoob/odin-recipes'
-  },
-  {
-    tech: ['HTML', 'CSS'],
-    title: 'Landing page',
-    description: 'Basic website using flexbox to layout the elements',
-    projectUrl: 'https://cookingnoob.github.io/landing-page-top-flexbox/',
-    repository: 'https://github.com/cookingnoob/landing-page-top-flexbox'
-  },
-  {
-    tech: ['HTML', 'CSS'],
-    title: 'Copycat of Tech store',
-    description: 'Web responsive tech store. It is as close as possible to the original tech store',
-    projectUrl: 'https://rtc-webresponsive-alancoste.netlify.app/',
-    repository: 'https://github.com/cookingnoob/web-responsive-rtc'
-  },
-  {
-    tech: ['HTML', 'CSS'],
-    title: 'Sign Up Form',
-    description: 'Simple sign up page made with CSS and HTML',
-    projectUrl: 'https://cookingnoob.github.io/sign-up-form-odin/',
-    repository: 'https://github.com/cookingnoob/sign-up-form-odin'
+    tech: ['Javascript', 'HTML', 'CSS'],
+    title: 'Dummy portfolio',
+    description: 'SPA Portfolio made only with Javascript, there are factories to create different DOM elements. Text content was created with ChatGPT, images with Midjourney',
+    projectUrl: 'https://rtc-portafoliovite.netlify.app',
+    repository: 'https://github.com/cookingnoob/rtc-vite-portafolio'
   },
   {
     tech: ['Javascript', 'HTML', 'CSS'],
@@ -91,11 +72,11 @@ export const programmingExp = [
     repository: 'https://github.com/cookingnoob/rtc-tienda'
   },
   {
-    tech: ['Javascript', 'HTML', 'CSS'],
-    title: 'Dummy portfolio',
-    description: 'SPA Portfolio made only with Javascript, there are factories to create different DOM elements. Text content was created with ChatGPT, images with Midjourney',
-    projectUrl: 'https://rtc-portafoliovite.netlify.app',
-    repository: 'https://github.com/cookingnoob/rtc-vite-portafolio'
+    tech: ['Typescript'],
+    title: 'Library',
+    description: 'Javascript page for adding books to a list. There is a factory of books asking for author, title, number of pages and if user read the book. I made a refactor of the code adding Typescript and simplified the code making it cleaner.',
+    projectUrl: 'https://github.com/cookingnoob/libraryOdin',
+    repository: 'https://github.com/cookingnoob/libraryOdin'
   },
   {
     tech: ['Javascript', 'CSS', 'HTML'],
@@ -119,10 +100,31 @@ export const programmingExp = [
     repository: 'https://github.com/cookingnoob/rock-paper-scissors2'
   },
   {
-    tech: ['Typescript'],
-    title: 'Library',
-    description: 'Javascript page for adding books to a list. There is a factory of books asking for author, title, number of pages and if user read the book. I made a refactor of the code adding Typescript and simplified the code making it cleaner.',
-    projectUrl: 'https://github.com/cookingnoob/libraryOdin',
-    repository: 'https://github.com/cookingnoob/libraryOdin'
+    tech: ['HTML', 'CSS'],
+    title: 'Copycat of Tech store',
+    description: 'Web responsive tech store. It is as close as possible to the original tech store',
+    projectUrl: 'https://rtc-webresponsive-alancoste.netlify.app/',
+    repository: 'https://github.com/cookingnoob/web-responsive-rtc'
+  },
+  {
+    tech: ['HTML', 'CSS'],
+    title: 'Sign Up Form',
+    description: 'Simple sign up page made with CSS and HTML',
+    projectUrl: 'https://cookingnoob.github.io/sign-up-form-odin/',
+    repository: 'https://github.com/cookingnoob/sign-up-form-odin'
+  },
+  {
+    tech: ['HTML', 'CSS'],
+    title: 'Landing page',
+    description: 'Basic website using flexbox to layout the elements',
+    projectUrl: 'https://cookingnoob.github.io/landing-page-top-flexbox/',
+    repository: 'https://github.com/cookingnoob/landing-page-top-flexbox'
+  },
+  {
+    tech: ['HTML'],
+    title: 'Recipe blog',
+    description: 'My first page ever! A simple food blog that renders my three favorite recipes. Images and recipes are mine!',
+    projectUrl: 'https://cookingnoob.github.io/odin-recipes/recipes/ribeye.html',
+    repository: 'https://github.com/cookingnoob/odin-recipes'
   },
 ]
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,9 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
 const About = () => {
+  const [more, setMore] = useState(false)
+
   return (
     <Box display={'flex'}>
       <Box
@@ -15,7 +17,27 @@ const About = () => {
           <Typography variant='h6' color={'primary'}>About me</Typography>
         </Box>
         <Box width={'70vw'}>
-          <Typography color={'primary'} fontSize={'35px'}>I’m a communicator transitioning to coding. I’m highly driven to solve problems as a team player. Research and critical thinking is how I reach solutions.</Typography>
+          {more ?
+            <>
+              <Typography color={'primary'} fontSize={'25px'}>
+                My name is Alan Coste, I have a Bachelor’s degree on Communications. I developed valuable skills such as researching and verification of information, understanding who is the audience and tailor a message for their needs without sacrificing the integrity of the concept.
+                <br />
+                <br />
+                I’m highly driven to learn and solve problems which is why my profesional background is so diverse. As a freelance photographer my main focus was to deliver editorial magazine quality images. I achieved this with a deep understanding of lighting, on set modifications and image editing with Photoshop.
+                <br />
+                <br />
+                During Covid I got into food business selling homemade gourmet products and cooking experiences for up to 10 people. This was possible thanks to research the best recipes and techniques to ensure a high quality product.
+                <br />
+                <br />
+                My expectations working on software development are to overcome hard problems, work with my team to deliver the most value as possible and to keep learning and growing in the tech world.
+              </Typography>
+              <Button onClick={() => setMore(false)}>Less info</Button>
+            </> :
+            <>
+              <Typography color={'primary'} fontSize={'35px'}>I’m a communicator transitioning to coding. I’m highly driven to solve problems as a team player. Research and critical thinking is how I reach solutions.</Typography>
+              <Button onClick={() => setMore(true)}>More info</Button>
+            </>
+          }
         </Box>
       </Box>
 
