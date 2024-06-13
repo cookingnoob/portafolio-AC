c8f13cfc376797d3b5db7aac0bf18e1493294b6d
cookingnoob <costealan@gmail.com>
2024-06-13 11:20:01 -0600

    add buttons to render education and job experience

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -1,41 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { education, jobExperience } from '../data/cv'
-import { Box, List, ListItem, Typography } from '@mui/material'
+import { Box, Button, List, ListItem, Typography } from '@mui/material'
 import { Circle } from '@mui/icons-material'
 
 const CV = () => {
-
+  const [content, setContent] = useState(null)
   return (
     <Box display={'flex'} >
-      <Box >
-        <Typography color={'primary'}>Professional Experience: </Typography>
-        {jobExperience.map((j, i) => (
-          <Box key={`${j}-${i}`} display={'flex'} flexDirection={'column'} marginBottom={'10px'}>
-            <Typography color={'primary'} variant='h6' marginTop={'20px'}>{j.position}</Typography>
-            <Typography color={'primary'}>{j.time}</Typography>
-            {j.description.map((d, i) => (
-              <Box key={`${d}-${i}`} display={'flex'}>
-                <Circle color='primary' sx={'height: 5px'} />
-                <Typography color={'primary'} marginLeft={'5px'} fontSize={'14px'}>{d}</Typography>
-              </Box >
-            ))}
-          </Box>
-        ))}
+      <Button onClick={() => setContent('job')}>Job Experience</Button>
+      <Button onClick={() => setContent('education')}>Education</Button>
+      {content === 'job' ?
+        <Box >
+          <Typography color={'primary'}>Professional Experience: </Typography>
+          {jobExperience.map((j, i) => (
+            <Box key={`${j}-${i}`} display={'flex'} flexDirection={'column'} marginBottom={'10px'}>
+              <Typography color={'primary'} variant='h6' marginTop={'20px'}>{j.position}</Typography>
+              <Typography color={'primary'}>{j.time}</Typography>
+              {j.description.map((d, i) => (
+                <Box key={`${d}-${i}`} display={'flex'}>
+                  <Circle color='primary' sx={'height: 5px'} />
+                  <Typography color={'primary'} marginLeft={'5px'} fontSize={'14px'}>{d}</Typography>
+                </Box >
+              ))}
+            </Box>
+          ))}
+        </Box>
+        : <></>}
 
-      </Box>
-      <Box>
-        <Typography color={'primary'}>Education: </Typography>
+      {content === 'education' ?
+        <Box>
+          <Typography color={'primary'}>Education: </Typography>
+          {education.map((e, i) => (
+            <Box key={`${e}-${i}`} display={'flex'} flexDirection={'column'}>
+              <Typography color={'primary'} variant='h6' marginTop={'20px'}>{e.title}</Typography>
+              <Typography color={'primary'}>{e.institution}</Typography>
+              <Typography color={'primary'}>{e.time}</Typography>
+              <Typography color={'primary'}>{e.description}</Typography>
+            </Box>
+          ))}
+        </Box>
+        :
+        <></>
+      }
 
-        {education.map((e, i) => (
-          <Box key={`${e}-${i}`} display={'flex'} flexDirection={'column'}>
-            <Typography color={'primary'} variant='h6' marginTop={'20px'}>{e.title}</Typography>
-            <Typography color={'primary'}>{e.institution}</Typography>
-            <Typography color={'primary'}>{e.time}</Typography>
-            <Typography color={'primary'}>{e.description}</Typography>
-          </Box>
-        ))}
-
-      </Box>
     </Box>
   )
 }
