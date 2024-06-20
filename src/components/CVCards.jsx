0a689253c723089483238569865118a8adbd3f85
cookingnoob <costealan@gmail.com>
2024-06-20 17:39:37 -0600

    refactor cv to include cvcard to render job experience

--- src/components/CVCards.jsx
+++ src/components/CVCards.jsx
@@ -5,29 +5,23 @@ const CVCards = ({ title, date, descriptions }) => {
   return (
     <Box width={'60vw'} padding={'10px'} >
       <Box display={'flex'} justifyContent={'space-between'}>
-        <Typography color={'primary'}>Freelance Photographer</Typography>
-        <Typography color={'primary'}>2018-PRESENT DAY</Typography>
+        <Typography color={'primary'}>{title}</Typography>
+        <Typography color={'primary'}>{date}</Typography>
       </Box>
       <Box width={'70vw'} display={'flex'} flexDirection={'column'} alignItems={'center'} marginTop={'10px'}>
         <Box display={'flex'} flexDirection={'column'} width={'50vw'}>
-          <Typography color={'primary'}>Real State photography and magazine quality Food Photography</Typography>
-          <br />
-          <Typography color={'primary'}>Achieve high quality image from camera with deep knowledge of lightning, camera gear, on site modifications such as food styling</Typography>
-          <br />
-          <Typography color={'primary'}>Post processing of images with Photoshop, enhancing image appeal without loosing credibility</Typography>
+          {descriptions.map((d, i) => (
+            <Box key={`${i}`}>
+              <Typography color={'primary'}>{d}</Typography>
+              <br />
+            </Box>
+          ))}
+
         </Box>
       </Box>
     </Box>
   )
 }
-/* Rectangle 12 */
 
-// position: absolute;
-// width: 1046px;
-// height: 423px;
-// left: 117px;
-// top: 108px;
-
-// background: #242424;
 
 export default CVCards
--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -7,49 +7,39 @@ import CVCards from '../components/CVCards'
 const CV = () => {
   const [content, setContent] = useState('job')
   return (
-    <>
-      <CVCards />
-    </>
-    // <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-    //   <Box width={'100vw'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-    //     <Button onClick={() => setContent('job')}>Job Experience</Button>
-    //     <Button onClick={() => setContent('education')}>Education</Button>
-    //   </Box>
-    //   {content === 'job' ?
-    //     <Box >
-    //       <Typography color={'primary'}>Professional Experience: </Typography>
-    //       {jobExperience.map((j, i) => (
-    //         <Box key={`${j}-${i}`} display={'flex'} flexDirection={'column'} marginBottom={'10px'}>
-    //           <Typography color={'primary'} variant='h6' marginTop={'20px'}>{j.position}</Typography>
-    //           <Typography color={'primary'}>{j.time}</Typography>
-    //           {j.description.map((d, i) => (
-    //             <Box key={`${d}-${i}`} display={'flex'}>
-    //               <Circle color='primary' sx={'height: 5px'} />
-    //               <Typography color={'primary'} marginLeft={'5px'} fontSize={'14px'}>{d}</Typography>
-    //             </Box >
-    //           ))}
-    //         </Box>
-    //       ))}
-    //     </Box>
-    //     : <></>}
 
-    //   {content === 'education' ?
-    //     <Box>
-    //       <Typography color={'primary'}>Education: </Typography>
-    //       {education.map((e, i) => (
-    //         <Box key={`${e}-${i}`} display={'flex'} flexDirection={'column'}>
-    //           <Typography color={'primary'} variant='h6' marginTop={'20px'}>{e.title}</Typography>
-    //           <Typography color={'primary'}>{e.institution}</Typography>
-    //           <Typography color={'primary'}>{e.time}</Typography>
-    //           <Typography color={'primary'}>{e.description}</Typography>
-    //         </Box>
-    //       ))}
-    //     </Box>
-    //     :
-    //     <></>
-    //   }
+    <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+      <Box width={'100vw'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+        <Button onClick={() => setContent('job')}>Job Experience</Button>
+        <Button onClick={() => setContent('education')}>Education</Button>
+      </Box>
+      {content === 'job' ?
+        <Box >
+          <Typography color={'primary'}>Professional Experience: </Typography>
+          {jobExperience.map((j, i) => (
+            <CVCards key={`${j}-${i}`} title={j.position} date={j.time} descriptions={j.description} />
 
-    // </Box>
+          ))}
+        </Box>
+        : <></>}
+
+      {content === 'education' ?
+        <Box>
+          <Typography color={'primary'}>Education: </Typography>
+          {education.map((e, i) => (
+            <Box key={`${e}-${i}`} display={'flex'} flexDirection={'column'}>
+              <Typography color={'primary'} variant='h6' marginTop={'20px'}>{e.title}</Typography>
+              <Typography color={'primary'}>{e.institution}</Typography>
+              <Typography color={'primary'}>{e.time}</Typography>
+              <Typography color={'primary'}>{e.description}</Typography>
+            </Box>
+          ))}
+        </Box>
+        :
+        <></>
+      }
+
+    </Box>
   )
 }
 
